60f4cfee81eec6a90a735610c25df43fe78f010a
senasamuel <samuelsena685@gmail.com>
2022-10-19 00:31:36 -0300

    Create 10. Expressõe_Booleanas.py

---
+++ 10. Expressõe_Booleanas.py
@@ -0,0 +1,78 @@
+A = 5
+B = 10
+C = 5
+
+if  A < B:
+    print("Expressão Verdadeira")
+else:
+    print("Expressão falsa")  
+
+print("Nova comparação")
+A = 5
+B = 10
+C = 5
+
+if  A > B:
+    print("Expressão Verdadeira")
+else:
+    print("Expressão falsa")
+print("Nova comparação")
+A = 5
+B = 10
+C = 5
+
+if  A <= B:
+    print("Expressão Verdadeira")
+else:
+    print("Expressão falsa")
+
+print("Nova comparação")
+A = 5
+B = 10
+C = 5
+
+if  A >= B:
+    print("Expressão Verdadeira")
+else:
+    print("Expressão falsa")
+print("Nova comparação") 
+
+A = 5
+B = 10
+C = 5
+
+if  A == B:
+    print("Expressão Verdadeira")
+else:
+    print("Expressão falsa")
+print("Nova comparação")  
+
+A = 5
+B = 10
+C = 5
+
+if  A < B and A == C:
+    print("Expressão Verdadeira")
+else:
+    print("Expressão falsa")
+print("Nova comparação")   
+
+A = 5
+B = 10
+C = 5
+
+if  A < B or A == C:
+    print("Expressão Verdadeira")
+else:
+    print("Expressão falsa")
+print("Nova comparação")    
+
+A = 5
+B = 10
+C = 5
+
+if  A < B or A != C:
+    print("Expressão Verdadeira")
+else:
+    print("Expressão falsa")
+print("Nova comparação")    
