b3e7dbe714b8ddcbcdc39246e78b324f11e7f66e
senasamuel <samuelsena685@gmail.com>
2022-10-18 23:54:28 -0300

    Create 9. Estruturas_Decisão.py

---
+++ 9. Estruturas_Decisão.py
@@ -0,0 +1,11 @@
+A = input("Informe a nota do aluno :")
+
+A = float(A)
+
+if A >= 7:
+    print("Aluno aprovado ")
+elif A >= 5:
+    print("Aluno de recuperação")    
+else:
+    print("Aluno foi reprovado.")    
+print("Final do semestre")    
